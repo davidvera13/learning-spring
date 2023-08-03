7769e3ecf43d6efaf0e60647f746892b6a9f25ae
davidvera13 <david.vera@9online.fr>
2023-08-03 18:42:42 +0200

    feat(spring): Spring properties DI using Annotations

---
+++ LEGACY - Spring and Hibernate for Beginners/section05 -  Dependency Injection - Annotations/lec11-spring-properties-dependency-injection/src/main/java/spring/learning/MainSpringApp.java
@@ -0,0 +1,33 @@
+package spring.learning;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import spring.learning.coach.Coach;
+
+public class MainSpringApp {
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext ctx =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Coach baseballCoach = ctx.getBean("baseballCoach", Coach.class);
+        Coach cricketCoach = ctx.getBean("cricketCoachBean", Coach.class);
+        Coach tennisCoach = ctx.getBean("tennisCoachBean", Coach.class);
+
+        // constructor dependency injection
+        System.out.println(baseballCoach.geDailyWorkout());
+        System.out.println(baseballCoach.getFortune());
+        System.out.println("****************");
+
+        // setter injection
+        System.out.println(cricketCoach.geDailyWorkout());
+        System.out.println(cricketCoach.getFortune());
+        System.out.println("****************");
+
+        // property injection
+        System.out.println(tennisCoach.geDailyWorkout());
+        System.out.println(tennisCoach.getFortune());
+        System.out.println("****************");
+        
+        ctx.close();
+    }
+}
---
+++ LEGACY - Spring and Hibernate for Beginners/section05 -  Dependency Injection - Annotations/lec11-spring-properties-dependency-injection/src/main/java/spring/learning/coach/impl/CricketCoach.java
@@ -0,0 +1,27 @@
+package spring.learning.coach.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import spring.learning.coach.Coach;
+import spring.learning.service.FortuneService;
+
+@Component("cricketCoachBean")
+public class CricketCoach implements Coach {
+    private FortuneService fortuneService;
+
+    // setter injection
+    @Autowired
+    public void setFortuneService(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
+    @Override
+    public String geDailyWorkout() {
+        return "Practice fast bowling for 15 minutes...";
+    }
+
+    @Override
+    public String getFortune() {
+        return fortuneService.getFortune();
+    }
+}
