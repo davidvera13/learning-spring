5fb832a0810fd01930b68fa49a939c50b3ae04f7
davidvera13 <david.vera@9online.fr>
2023-08-21 22:49:31 +0200

    feat(aop): after returning startup project

---
+++ LEGACY - Spring and Hibernate for Beginners/section11 - Aspect Oriented Programming AOP/lec33-aop-after-returning/src/main/java/spring/learning/aop/dao/AccountDao.java
@@ -0,0 +1,60 @@
+package spring.learning.aop.dao;
+
+import org.springframework.stereotype.Component;
+import spring.learning.aop.domain.Account;
+
+@Component
+public class AccountDao {
+    private String name;
+    private String serviceCode;
+
+    public void addAccount() {
+        System.out.println(getClass() + " is doing its db work: add customer\n");
+    }
+
+    public void addAccount(Account account) {
+        System.out.println(getClass() + " is doing its db work: add customer account\n");
+    }
+
+    public void addAccount(Account account, boolean isVip) {
+        System.out.println(getClass() + " is doing its db work: add customer account + VIP\n");
+    }
+
+    public void getAccount() {
+        System.out.println(getClass() + " is getting customer details\n");
+    }
+
+    public void updateAccount() {
+        System.out.println(getClass() + " is updating customer account\n");
+    }
+
+    public boolean updateAccountDetails() {
+        System.out.println(getClass() + " is updating customer details\n");
+        return true;
+    }
+
+    public boolean doWork() {
+        System.out.println(getClass() + " doWork()\n");
+        return true;
+    }
+
+    public String getName() {
+        System.out.println(getClass() + ": in getName()\n");
+        return name;
+    }
+
+    public void setName(String name) {
+        System.out.println(getClass() + ": in setName()\n");
+        this.name = name;
+    }
+
+    public String getServiceCode() {
+        System.out.println(getClass() + ": in getServiceCode()\n");
+        return serviceCode;
+    }
+
+    public void setServiceCode(String serviceCode) {
+        System.out.println(getClass() + ": in setServiceCode()\n");
+        this.serviceCode = serviceCode;
+    }
+}
---
+++ LEGACY - Spring and Hibernate for Beginners/section11 - Aspect Oriented Programming AOP/lec33-aop-after-returning/src/main/java/spring/learning/aop/domain/Account.java
@@ -0,0 +1,13 @@
+package spring.learning.aop.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class Account {
+    private String name;
+    private String level;
+}
